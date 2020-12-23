3d09f21288cf452c66093ac8541a18f157d195c7
Joshua Lessie <wisemanjosh@outlook.com>
2020-12-23 01:59:20 +0000

    Random Password Generator

---
+++ main.py
@@ -0,0 +1,9 @@
+# Password Generator
+# by Josh Wiseman Lessie
+
+import string as s
+from random import *
+ch = s.ascii_letters + s.digits + s.punctuation
+password = "".join(choice(ch) for x in range(randint(8, 16)))
+
+print(password)
